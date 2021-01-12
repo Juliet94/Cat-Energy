f7941c46ff5ac2db1adfb5f452f36b751a57fe07
Anastasia <70826807+Juliet94@users.noreply.github.com>
2021-01-13 00:17:10 +0300

    Автоматизация сборки

---
+++ source/js/script.js
@@ -0,0 +1,10 @@
+const mainMenu = document.querySelector('.page-header');
+const buttonToggle = document.querySelector('.page-header__toggle');
+
+mainMenu.classList.remove('page-header--menu-nojs');
+mainMenu.classList.add('page-header--menu-closed');
+
+buttonToggle.addEventListener('click', function () {
+  mainMenu.classList.toggle('page-header--menu-closed');
+  mainMenu.classList.toggle('page-header--menu-open');
+})
