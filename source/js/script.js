05e7500d8e3a8995864f0c0d1d8cceb6cef77ac6
Anastasia <70826807+Juliet94@users.noreply.github.com>
2021-01-14 22:01:51 +0300

    Испралвено меню

--- source/js/script.js
+++ source/js/script.js
@@ -1,10 +1,13 @@
 const mainMenu = document.querySelector('.page-header');
 const buttonToggle = document.querySelector('.page-header__toggle');
+const menuList = document.querySelector('.header-nav');
 
 mainMenu.classList.remove('page-header--menu-nojs');
 mainMenu.classList.add('page-header--menu-closed');
+menuList.classList.add('header-nav--menu-closed');
 
 buttonToggle.addEventListener('click', function () {
   mainMenu.classList.toggle('page-header--menu-closed');
   mainMenu.classList.toggle('page-header--menu-open');
+  menuList.classList.toggle('header-nav--menu-closed');
 })
