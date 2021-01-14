62e2e55bbd1be7fcf3cc0c312cd82019fe945049
Anastasia <70826807+Juliet94@users.noreply.github.com>
2021-01-14 06:49:28 +0300

    Изменен скрипт

--- source/js/script.js
+++ source/js/script.js
@@ -2,13 +2,10 @@ const mainMenu = document.querySelector('.page-header');
 const buttonToggle = document.querySelector('.page-header__toggle');
 
 mainMenu.classList.remove('page-header--menu-nojs');
+mainMenu.classList.add('page-header--menu-closed');
 
-if( window.innerWidth < 768 ){
-  mainMenu.classList.add('page-header--menu-closed');
-  buttonToggle.addEventListener('click', function () {
-    mainMenu.classList.toggle('page-header--menu-closed');
-    mainMenu.classList.toggle('page-header--menu-open');
-  })
-} else {
-  mainMenu.classList.add('page-header--menu-open');
-}
+buttonToggle.addEventListener('click', function () {
+  mainMenu.classList.toggle('page-header--menu-closed');
+  mainMenu.classList.toggle('page-header--menu-open');
+})
+
